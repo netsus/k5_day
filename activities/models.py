d8a15f221fb69273b90760f8daebd3dbb372cc76
netsus <cwjcsk@naver.com>
2022-08-10 13:06:03 +0900

    Activity에서 Phto 테이블 분리 하나의 Activity에 여러 사진 저장할 수도 있고, 사진에 대한 제목(caption)이 들어갈 수 도 있다고 판단

--- activities/models.py
+++ activities/models.py
@@ -1,10 +1,19 @@
 from django.conf import settings
 from django.db import models
 
+class Photo(models.Model):
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=100)
+    file = models.ImageField(upload_to="activity_photos")
+    activity = models.ForeignKey("Activity", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
 class Activity(models.Model):
     title=models.CharField(max_length=50)
     description=models.TextField()
-    photo=models.ImageField()
     liked_user_set = models.ManyToManyField(
         settings.AUTH_USER_MODEL, blank=True, related_name="liked_activity_set"
     )
