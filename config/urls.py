53c3acb30b1e07f8f961065a46364f274476560f
netsus <cwjcsk@naver.com>
2022-08-12 01:46:21 +0900

    django rest framework basic setting

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.urls import path, include
 from django.views.generic import RedirectView
 
@@ -23,7 +24,32 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("activities/", include("activities.urls")),
-    path("", RedirectView.as_view(pattern_name="activities:list"), name="root"),
+    # path("", RedirectView.as_view(pattern_name="activities:list"), name="root"),
+    # DRF
+    # path('api-auth/', include('rest_framework.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+
+User = get_user_model()
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns += [path('', include(router.urls))]
