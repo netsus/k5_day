5579046abf874f0fae84f20a837bad9dcd7e799d
netsus <cwjcsk@naver.com>
2022-08-13 10:47:31 +0900

    회원가입시 자동 로그인 기능 설정

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,28 @@
 from django.conf import settings
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.context_processors import messages
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView, UpdateView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, TemplateView, UpdateView, FormView
 
 from accounts.forms import SignupForm, ProfileForm
 
 
-class SignupView(CreateView):
+class SignupView(FormView):
     form_class = SignupForm
     template_name = "form.html"
-    success_url = settings.LOGIN_URL
+    # success_url = reverse("activities:list")
+
+    def form_valid(self, form):
+        form.save()
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return HttpResponseRedirect(reverse("activities:list"))
 
 class MyLoginView(LoginView):
     template_name = "form.html"
